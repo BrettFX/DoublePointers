e394800f0e51001da05713b46ccff027e8701f66
Brett Allen <brettallen777@gmail.com>
2017-05-30 14:37:16 -0400

    Implementing memory mapping function for generic double pointers

--- DoublePointers.cpp
+++ DoublePointers.cpp
@@ -5,8 +5,25 @@
 
 using namespace std;
 
+template<class T>
+void displayMemoryTable(T** dblPtr);
+
 int main(int argc, char** argv){
-	DPLinkedList test;
+	int x = 21;
+	int* intPtr = &x;
+	int** dblIntPtr = &intPtr;
+	
+	displayMemoryTable(dblIntPtr);
 	
 	return 0;
+}
+
+/**
+Displays a descriptive table mapping the memory addresses of the double pointer in question
+
+@param T** the generic type double pointer to be tested
+*/
+template<class T>
+void displayMemoryTable(T** dblPtr){
+	cout << **dblPtr << "\n";
 }
