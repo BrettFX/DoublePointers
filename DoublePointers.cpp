0391e111ae0145eda2e30484882ebdde70751900
Brett Allen <brettallen777@gmail.com>
2017-05-30 20:09:27 -0400

    Final implementation for memory table

--- DoublePointers.cpp
+++ DoublePointers.cpp
@@ -25,11 +25,11 @@ Displays a descriptive table mapping the memory addresses of the double pointer 
 */
 template<class T>
 void displayMemoryTable(T** dblPtr){
-	cout << "+********+********+********+*******+\n";
-	cout << "|Variable| Degree | Address| Data  |\n";
-	cout << "|********|********|********|*******|\n";
-	cout << "|        |        |        |       |\n";
-	cout << "|********|********|********|*******|\n";
-	cout << "|        |        |        |       |\n";
-	cout << "|********|********|********|*******|\n";
+	cout << "|**************|**************|**************|**************|\n";
+	cout << "|Variable      | Degree       | Address Ptr  | Data         |\n";
+	cout << "|**************|**************|**************|**************|\n";
+	cout << "|" << &dblPtr << "|     " << 2 << "        |" << dblPtr << "|" << *dblPtr << "|\n";
+	cout << "|**************|**************|**************|**************|\n";
+	cout << "|" << dblPtr << "|     " << 1 << "        |" << *dblPtr << "|     " << **dblPtr << "       |\n";
+	cout << "|**************|**************|**************|**************|\n";
 }
